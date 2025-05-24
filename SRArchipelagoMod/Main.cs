9d742fc1949efe8e374facbf37f69de858e2eaca
EepyKitty <eepykibby@gmail.com>
2025-05-24 16:03:36 +0800

    Add project files.

---
+++ SRArchipelagoMod/Main.cs
@@ -0,0 +1,35 @@
+﻿using SRML;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SRArchipelagoMod
+{
+    public class Main : ModEntryPoint
+    {
+        // Called before GameContext.Awake
+        // You want to register new things and enum values here, as well as do all your harmony patching
+        public override void PreLoad()
+        {
+            HarmonyInstance.PatchAll();
+        }
+
+
+        // Called before GameContext.Start
+        // Used for registering things that require a loaded gamecontext
+        public override void Load()
+        {
+
+        }
+
+        // Called after all mods Load's have been called
+        // Used for editing existing assets in the game, not a registry step
+        public override void PostLoad()
+        {
+
+        }
+
+    }
+}
